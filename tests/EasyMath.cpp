9daab898cb4f7e6f38e3d920341c2608bce9eac9
luiz-couto <luiz.felipe.556@outlook.com>
2021-12-02 09:17:38 -0300

    circle class and tests

--- tests/EasyMath.cpp
+++ tests/EasyMath.cpp
@@ -1,25 +1,6 @@
 #include <gtest/gtest.h>
-#include "EasyMath.hpp"
 
-TEST(EasyMathSquareTest, SquareNoNegativeSide) {
-    EXPECT_THROW({
-        try
-        {
-            square<double> s = square<double>(-1);
-        }
-        catch(const std::invalid_argument& e )
-        {
-            EXPECT_STREQ( "cannot crate a square with a negative side", e.what() );
-            throw;
-        }
-    }, std::invalid_argument);
-}
-
-TEST(EasyMathSquareTest, Area) {
-    square<double> a = square<double>(36);
-    ASSERT_EQ(1296, a.area());
-}
- 
+#include "Circle.cpp"
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
